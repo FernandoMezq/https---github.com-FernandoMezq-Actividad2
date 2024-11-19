89243b22c5f6624c7cc82763fc7e0ef0be6095b1
Fernando Mezquida <fernando.mezquida@educamadrid.org>
2024-11-19 10:46:55 +0100

    añadimos al template un div con el titulo de la pagina, y le damos un estilo cambiando la fuente la alineacion y el margen

--- TodoList/src/App.vue
+++ TodoList/src/App.vue
@@ -14,44 +14,19 @@ export default {
 
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <h1>Lista de Tareas</h1>
+    <TodoList/>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+#app{
+font-family: 'Times New Roman', Times, serif;
+text-align: center;
+margin-top 25px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
