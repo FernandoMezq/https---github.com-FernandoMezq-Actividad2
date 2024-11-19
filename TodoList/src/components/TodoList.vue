b4dee61bc5dbbc8620e01c03002947a287bd32e8
Fernando Mezquida <fernando.mezquida@educamadrid.org>
2024-11-19 11:40:10 +0100

    Creo el inputtarea.vue y en todo list en el script comienzo a hacer la lista

---
+++ TodoList/src/components/TodoList.vue
@@ -0,0 +1,30 @@
+<script>
+import InputTarea from './InputTarea.vue';
+
+export default{
+    name: 'TodoList',
+    components:{
+        InputTarea
+    },
+data(){
+    return{
+        tasks:[]
+    };
+},
+}
+
+
+</script>
+
+<template>
+
+
+
+
+
+</template>
+
+<style>
+
+
+</style>
